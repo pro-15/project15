fbbc1d4dbc171d06ef3312e9a24e029d452f120c
user-014 <thakkudus04@gmail.com>
2023-10-30 20:51:49 +0530

    sql dumb

--- config/db/project15.sql
+++ config/db/project15.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 29, 2023 at 03:50 PM
+-- Generation Time: Oct 30, 2023 at 03:20 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -423,19 +423,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `phone` char(10) NOT NULL,
   `password` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `gender`, `age`, `email`, `phone`, `password`) VALUES
-(1, 'Abhishek', 'M', 45, 'abhi@gmail.com', '1234567891', '12345678'),
+(1, 'Abhishek', 'M', 45, 'abhi@gmail.com', '1234567890', '12345678'),
 (8, 'Ajith k s', 'M', 46, 'ajith12@gmail.com', '1234567891', '12345678'),
-(9, 'Ajith k s', 'M', 34, 'ajith123456@gmail.com', '1234567891', '12345678'),
-(10, 'Yaseen', 'F', 20, 'yasee@gmail.com', '2134567890', '12345678'),
-(11, 'Johan Liebert', 'M', 23, 'jl@mon.str', '9999999999', '123');
+(9, 'Ajith k s', 'M', 34, 'ajith123456@gmail.com', '1234567892', '12345678'),
+(10, 'Yaseen', 'F', 20, 'yasee@gmail.com', '1234567893', '12345678'),
+(12, 'Johan Liebert', 'M', 23, 'jl@mon.str', '1234567894', '123');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
